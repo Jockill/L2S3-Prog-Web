35c64b3d448007d7615b24eba5866b13be65dce8
Jockill <hugo.jochymski@etu.unistra.fr>
2020-10-03 12:27:18 +0200

    Reorganisation de la structure, ajout d'un master header et footer, MaJ des ressources.

---
+++ pages/contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+
+<!-- < ?php include("header.html") ?> -->
+
+<body>
+
+    <main>
+        <form action="" method="post">
+            <label for="nom">Nom</label>
+            <input type="text" name="nom" id="nom">
+            <br>
+            <label for="prenom">Prenom</label>
+            <input type="text" name="prenom" id="prenom">
+            <br> <!--
+            <label for="email">Mail</label>
+            <input type="text" name="email" id="email">
+            <br>
+            <label for="message">Message</label>
+            <textarea name="message" id="message" cols="30" rows="10"></textarea> -->
+            <button type="submit">Click</button>
+        </form>
+
+    </main>
+
+<script src="../script/script.js"></script>
+
+</body>
+</html>
---
+++ pages/footer.php
@@ -0,0 +1,76 @@
+<!-- PENSER A ENLEVER -->
+<link rel="stylesheet" href="../css/master.css">
+
+    <footer>
+        <div> <!-- Gauche -->
+            <form action="">
+                <label for="signup">Newsletter</label><br>
+                <input type="text" name="mail" id="mail">
+                <button type="submit">S'abonner</button>
+            </form>
+
+            <div> <!-- Reseaux -->
+                <ul>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/fb-icon.png" alt="Facebook"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/twitter-icon.png" alt="Twitter"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/ig-icon.png" alt="Instagram">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/reddit-icon.png" alt="Reddit">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/discord-icon.png" alt="Discord">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/steam-icon.png" alt="Steam">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="../ressources/onlyF-icon.png" alt="OnlyFans">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div> <!-- Millieu -->
+            <h3>Notre mission</h3>
+            <p>Nous nous battons chaque jour auprès des L33t pour subvenir à leurs besoins sanitaires, n'importe quand, n'importe où.</p>
+        </div>
+
+        <div> <!-- Droite -->
+
+        </div>
+        <!--
+            Copyright
+            Logo
+            Réseaux
+            Téléphone
+            Powered by
+               Subscribe
+                Press
+                Our mission
+            Awards and certifications
+            Témoignage
+            Don
+         -->
+
+    </footer>
+</html>
