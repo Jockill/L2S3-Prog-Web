c10c22df0eedbca50c01ab015d1db83c09c1b929
jockill <jockill@parrot>
2020-11-24 21:31:28 +0100

    Ajout d'un index

---
+++ pages/index.php
@@ -0,0 +1,11 @@
+<?php include "header.php" ?>
+<link rel="stylesheet" href="../css/style_index.css">
+
+<main>
+	<h2>Bienvenue !</h2>
+	<p>
+		Il fallait un titre et un peu de texte pour pas que ca paraisse vide.
+	</p>
+</main>
+
+<?php include "footer.php" ?>
