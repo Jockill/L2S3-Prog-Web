351c9f8ab1ee416e3e8c39a76e9c42396a17c3e1
jockill <jockill@parrot>
2020-11-24 21:23:01 +0100

    Oupsi

---
+++ pages/presentation.php
@@ -0,0 +1,77 @@
+<?php include_once "./header.php" ?>
+<link rel="stylesheet" href="../css/style_presentation.css">
+    <main>
+        <h2>Personnages importants</h2>
+
+        <div id="cartes">
+            <div class="portrait">
+                <h5>Mr. R0bøt</h5>
+                <div>
+                    99
+                </div>
+                <div>
+                    <img src="../ressources/mrrobot.jpg" alt="">
+                    <p>
+                        Il prétend tenir son énergie d'une pomme de terre et qu'il brulera votre maison avec des citrons... <br><br>
+                        Hormis ça, il cuisine très bien les gateaux.
+                    </p>
+                </div>
+                <div>
+                    99
+                </div>
+            </div>
+
+            <div class="portrait">
+                <h5>Hackerman</h5>
+                <div>
+                    3.14
+                </div>
+                <div>
+                    <img src="../ressources/hackerman.png">
+                    <p>
+                        <b>"Hackerman, he's the most powerful hacker of all times".</b><br><br>
+                        Vous vous souvenez surement de lui pour avoir hacké le temps dans <i>Kung Fury</i>.<br><br>
+                        Psst... Clique <a href="https://www.youtube.com/watch?v=KEkrWRHCDQU">ici</a> et ça reste entre nous ;)
+                    </p>
+                </div>
+                <div>
+                    42
+                </div>
+            </div>
+
+            <div class="portrait">
+                <h5>Zelda</h5>
+                <div>
+                    420
+                </div>
+                <div>
+                    <img src="../ressources/zelda.jpg">
+                    <p>
+                        Il est prêt a parier que tu n'as même pas fait attention au fait que c'est écrit Zelda eu lieu de <em>LINK</em>.
+                        <br><br>
+                        Il a bon dos Zelda...
+                    </p>
+                </div>
+                <div>
+                    16
+                </div>
+            </div>
+    </div>
+
+        <h2>Nous retrouver</h2>
+        <div id="map">
+            <p>
+                Tu vois le gros rocher ? Bah tu continues tout droit sur 20km. <br>
+                Après ca, tu tournes au 974321e buisson puis tu prends la tangente avec la craquelure dans la chaussée (tu sais celle qui est restée après les travaux, ceux après lesquels mamie se plaint tout le temps ...).
+                Tu la recouvre de poudre d'escampette et puis à la troisième sortie tu y es presque t'en fais pas.
+                Il faut juste faire 3 tours sur toi meme en sensi 12 dans le sens trigo. Et enfin, sans perdre le nord tu touches le mlg noscope 420 blaze it across-the-street et c'est juste après le lampadaire. <br><br>
+                Si tu n'as pas suivi il y a une carte google maps juste à ta droite. :) 
+            </p>
+                <div>
+                    <iframe width="300" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=L33T%20LTD%20manchester&t=&z=5&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
+        </div>
+
+    </main>
+
+<?php include_once "./footer.php" ?>
